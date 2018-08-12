8452b68df70a030013a6672b37b2a7c459147901
juacy willian <juacy.willian@gmail.com>
2018-08-12 12:49:44 -0300

    initial commit

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}app.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}app.py
@@ -6,7 +6,8 @@ class {{cookiecutter.repo_name|capitalize}}App(App):
 
     Edit {{cookiecutter.repo_name}}.kv to get started.
     """
+    theme_cls = ThemeManager()
+    theme_cls.primary_palete = 'Teal'
 
     def build(self):
         return self.root
-
