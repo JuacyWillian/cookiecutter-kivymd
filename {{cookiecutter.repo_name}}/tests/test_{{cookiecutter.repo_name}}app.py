8069136ce5401e999e652412e68a817f4c6b8e20
Juacy Willian <juacy.willian@gmail.com>
2018-10-06 09:53:38 -0300

    add test

--- {{cookiecutter.repo_name}}/tests/test_{{cookiecutter.repo_name}}app.py
+++ {{cookiecutter.repo_name}}/tests/test_{{cookiecutter.repo_name}}app.py
@@ -2,21 +2,25 @@
 # -*- coding: utf-8 -*-
 
 import unittest
+from kivy.clock import Clock
+from {{cookiecutter.repo_name}}.{{cookiecutter.repo_name}}app import {{cookiecutter.repo_name | capitalize}}App
 
-from {{cookiecutter.repo_name}}.{{cookiecutter.repo_name}}app import {{cookiecutter.repo_name|capitalize}}App
 
-
-class Test{{cookiecutter.repo_name|capitalize}}App(unittest.TestCase):
+class Test{{cookiecutter.repo_name | capitalize}}App(unittest.TestCase):
     """TestCase for {{cookiecutter.repo_name|capitalize}}App.
     """
+
     def setUp(self):
-        self.app = {{cookiecutter.repo_name|capitalize}}App()
+        self.app = {{cookiecutter.repo_name | capitalize}}App()
+        Clock.schedule_once(lambda *x: self.app.stop(), 0.000001)
+        self.app.run()
 
     def test_name(self):
         self.assertEqual(self.app.name, '{{cookiecutter.repo_name}}')
 
     def tearDown(self):
         pass
+
 
 if __name__ == '__main__':
     unittest.main()
